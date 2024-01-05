f8274f9418df19c6584a29b39a5f123e42d8b9bc
Elisha Naik <ramavath@aptagrim.com>
2024-01-05 18:00:00 +0530

    comfy store initial commit

---
+++ src/App.jsx
@@ -0,0 +1,78 @@
+import { About,Error,HomeLayout,Landing,Products,SingleProduct,Cart,Checkout,Orders,Login,Register} from './pages/index'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+
+//errorelement
+import {ErrorElement} from './components/index';
+
+//loader
+import {loader as landingLoader} from './pages/Landing'
+import {loader as singleProductLoader} from './pages/SingleProduct'
+
+export default function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <HomeLayout />,
+      errorElement: <Error />,
+      children:[
+        {
+          index: true,
+          element: <Landing />,
+          errorElement: <ErrorElement/>,
+          loader: landingLoader
+        },
+        {
+          path: 'products',
+          element: <Products />,
+
+          
+        },
+        {
+          path: 'products/:productsId',
+          element: <SingleProduct />,
+          errorElement: <ErrorElement/>,
+          loader: singleProductLoader
+        },
+        {
+          path: 'cart',
+          element: <Cart />
+        },
+        {
+          path: 'checkout',
+          element: <Checkout/>
+        },
+        {
+          path: 'about',
+          element: <About />
+        },
+        {
+          path: 'orders',
+          element:<Orders />
+
+        }
+      ]
+
+    },
+    {
+      path: 'login',
+      element: <Login/>,
+      errorElement: <Error />
+    },
+    {
+      path: 'register',
+      element: <Register/>,
+      errorElement: <Error />
+    }
+
+  ])
+
+
+
+  
+  return (
+    
+    <RouterProvider router={router}/>
+    
+  )
+}
---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+export {default as FormInput} from './formInput'
+export {default as SubmitButton} from './submitButton'
+export {default as Header} from './header'
+export {default as Navbar} from './navbar'
+export {default as ErrorElement} from './errorElement'
+export {default as SectionTitle} from './sectionTitle'
+export {default as ProductsGrid} from './productsGrid'
+export {default as FeaturedProducts} from './featuredProducts'
+
+
+
+
